5045cd21ec78809b027dc3f4666c7a9a2dd0d579
itzkimmy <wanhakimi157@gmail.com>
2023-11-10 16:31:41 +0800

    first commit

---
+++ database/alamanda.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 10, 2023 at 09:31 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `alamanda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+-- Error reading structure for table alamanda.admin: #1932 - Table &#039;alamanda.admin&#039; doesn&#039;t exist in engine
+-- Error reading data for table alamanda.admin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `alamanda`.`admin`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+-- Error reading structure for table alamanda.books: #1932 - Table &#039;alamanda.books&#039; doesn&#039;t exist in engine
+-- Error reading data for table alamanda.books: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `alamanda`.`books`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+-- Error reading structure for table alamanda.comments: #1932 - Table &#039;alamanda.comments&#039; doesn&#039;t exist in engine
+-- Error reading data for table alamanda.comments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `alamanda`.`comments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issue_book`
+--
+-- Error reading structure for table alamanda.issue_book: #1932 - Table &#039;alamanda.issue_book&#039; doesn&#039;t exist in engine
+-- Error reading data for table alamanda.issue_book: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `alamanda`.`issue_book`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Error reading structure for table alamanda.user: #1932 - Table &#039;alamanda.user&#039; doesn&#039;t exist in engine
+-- Error reading data for table alamanda.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `alamanda`.`user`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
